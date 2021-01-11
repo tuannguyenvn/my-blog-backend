ec4268a463adb75a807db060def95244c34aa579
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-11 12:49:12 +0100

    Add bodyParser-Paket , Response from Server to Json Request

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express, { request, response } from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
 app.get('/hello', (request, response) => response.send('Hello'));
+app.post('/hello', (request, response) => response.send(`Hello ${request.body.name} `));
+
 
 app.listen(8000, () => console.log('Listening in Port 8000'));
