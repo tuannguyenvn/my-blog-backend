95ea02b2356a288924fa0a5d231a307019fdb002
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-15 16:11:53 +0100

    update value from data in db (upvotes )

--- src/server.js
+++ src/server.js
@@ -8,10 +8,29 @@ app.use(bodyParser.json());
 app.get('/hello', (req, res) => res.send('Hello'));
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name} `));
-app.post('/articles/:name/upvote', (req, res) => {
-    const artikelName = req.params.name;
-    articlesInfo[artikelName].upvotes += 1;
-    res.status(200).send(`${artikelName} now has ${articlesInfo[artikelName].upvotes}`);
+
+app.post('/api/articles/:name/upvote', async(req, res) => {
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017',  {useNewUrlParser: true} );
+        const db = client.db('my-blog');        
+
+        const articlesInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes : articlesInfo.upvotes +1 ,
+            },           
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    
+        res.status(200).json(updatedArticleInfo);
+    
+        client.close();        
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });         
+    }
 });
 // app.post('/articles/:name/add-comment', (req, res) => {
 //     const artikelName = req.params.name;
