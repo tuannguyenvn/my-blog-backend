5662eee45a4664bd9c84027ade6f6526d96d49e0
tuannguyenvn <tuannguyen.de@gmail.com>
2021-01-15 15:01:07 +0100

    mongoDb connection, return search object from db

--- src/server.js
+++ src/server.js
@@ -1,20 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: []
-    },
-    'my-thoughts-on-resumes': {
-        upvotes: 0,
-        comments: []
-    },
-};
+import MongoClient from 'mongodb';
 
 const app = express();
 
@@ -27,12 +13,28 @@ app.post('/articles/:name/upvote', (req, res) => {
     articlesInfo[artikelName].upvotes += 1;
     res.status(200).send(`${artikelName} now has ${articlesInfo[artikelName].upvotes}`);
 });
-app.post('/articles/:name/add-comment', (req, res) => {
-    const artikelName = req.params.name;
-    const {username, text} = req.body;
-    articlesInfo[artikelName].comments.push({username, text});
-    res.status(200).send(articlesInfo[artikelName]);
+// app.post('/articles/:name/add-comment', (req, res) => {
+//     const artikelName = req.params.name;
+//     const {username, text} = req.body;
+//     articlesInfo[artikelName].comments.push({username, text});
+//     res.status(200).send(articlesInfo[artikelName]);
+// });
+
+app.get('/api/articles/:name', async(req, res) => {
+  
+    try {
+        const articleName = req.params.name;
+
+        const client = await MongoClient.connect('mongodb://localhost:27017',  {useNewUrlParser: true} );
+        const db = client.db('my-blog');
+
+        const articlesInfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(articlesInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error }); 
+    }
+
 });
-
-
 app.listen(8000, () => console.log('Listening in Port 8000'));
